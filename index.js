a9c99fd446e8d053505272a5c8f27232acdcf72c
Oscar Harron <oscarharron@hotmail.com>
2022-12-17 15:50:35 +1300

    setup working post request

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
--- public/utils.js
+++ public/utils.js
@@ -1,19 +1,31 @@
-function submitForm(event) {
-  event.preventDefault();
-
-  console.log(event.target);
-}
-
-const form = document.querySelector('#form');
+const form = document.querySelector('form');
 const feedback = document.querySelector('#feedback');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  getData(e.target);
-});
+function submitForm(event) {
+  event.preventDefault();
+  getData(event.target);
+  console.log(event.target);
+}
 
 function getData(form) {
   let formData = new FormData(form);
   let feedback = Object.fromEntries(formData);
   console.log(feedback);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendData();
+});
+
+function sendData() {
+  let fee = feedback.value;
+  console.log(fee);
+  fetch('/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ feedback: fee }),
+  });
 }
