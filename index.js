ab880de21db1da500bc3e926ba073245bc787dc5
Oscar Harron <oscarharron@hotmail.com>
2022-12-18 22:22:45 +1300

    add id to post request

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Post feedback to DynamoDB on post
 app.post('/', (req, res) => {
   console.log(req.body);
+  const feedback = req.body;
+  feedback.id = 2;
+  console.log(feedback);
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
