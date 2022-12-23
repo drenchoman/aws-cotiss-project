0432776e9a4b886f39a0ec897778fa165cd3306b
Oscar Harron <oscarharron@hotmail.com>
2022-12-23 17:25:47 +1300

    restart, set up node as api server

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const PORT = 5000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.json({ message: 'Hello World' });
 });
 
-// Post feedback to DynamoDB on post
-app.post('/', (req, res) => {
-  console.log(req.body);
-  const feedback = req.body;
-  feedback.id = 2;
-  console.log(feedback);
-  let response = JSON.stringify(feedback);
-  res.status(200).json({ message: 'Success' });
+app.get('/health-check', (req, res) => {
+  res.json({ message: 'Server up and running' });
 });
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(PORT, () => {
+  console.log('Server Running on PORT', PORT);
+});
